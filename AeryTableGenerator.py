ac28b753141cf852832b72a938d31157dd044e7b
romanceformoon <lmwljw98@gmail.com>
2024-03-13 20:50:25 +0900

    update

--- AeryTableGenerator.py
+++ AeryTableGenerator.py
@@ -4,7 +4,7 @@ import sys
 
 import requests
 
-table_data = requests.get('https://hibyethere.github.io/table/data.json')
+table_data = requests.get('https://asumatoki.kr/table/aery/data.json')
 input_json = table_data.json()
 
 level_1 = [x for x in input_json if x['folder'] == 'LEVEL 1']
@@ -28,6 +28,7 @@ level_18 = [x for x in input_json if x['folder'] == 'LEVEL 18']
 level_19 = [x for x in input_json if x['folder'] == 'LEVEL 19']
 level_20 = [x for x in input_json if x['folder'] == 'LEVEL 20']
 level_dummy = [x for x in input_json if x['folder'] == 'LEVEL DUMMY']
+level_old = [x for x in input_json if x['folder'] == 'OLD CHARTS']
 
 db_path = ''
 if getattr(sys, 'frozen', False):
@@ -112,28 +113,44 @@ for e in level_20:
 for e in level_dummy:
     cur.executemany('INSERT INTO Aery VALUES (?, ?, ?)',
                     [(e['md5'], e['folder'], e['level'])])
+for e in level_old:
+    cur.executemany('INSERT INTO Aery VALUES (?, ?, ?)',
+                    [(e['md5'], e['folder'], e['level'])])
     
 conn.commit()
 conn.close()
 
-header_data = requests.get('https://hibyethere.github.io/table/header.json')
+header_data = requests.get('https://asumatoki.kr/table/aery/header.json')
 last_update = header_data.json()['last_update']
 
-table_folder = f'AeryTable-{last_update}'
+table_folder = 'AeryTable'
 isExist = os.path.exists(table_folder)
 if not isExist:
     os.makedirs(table_folder) 
 
-level_list = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','DUMMY']
+level_list = ['1', '2', '3', '4', 
+              '5', '6', '7', '8', 
+              '9', '10', '11', '12', 
+              '13', '14', '15', '16', 
+              '17', '18', '19', '20', 
+              'DUMMY', 'OLD']
 
 for level in level_list:
-    lr2folder = [f"#COMMAND song.hash   in (SELECT md5 FROM Aery WHERE folder = 'LEVEL {level}') \n", "#MAXTRACKS 0\n", "#CATEGORY Aery 5key\n", f"#TITLE LEVEL {level}\n", f"#INFORMATION_A Last update: {last_update}\n", "#INFORMATION_B\n"]
     if level == 'DUMMY':
+        lr2folder = [f"#COMMAND song.hash   in (SELECT md5 FROM Aery WHERE folder = 'LEVEL DUMMY') \n", "#MAXTRACKS 0\n", "#CATEGORY [5KEYS AERY]\n", f"#TITLE LEVEL DUMMY\n", f"#INFORMATION_A LAST UPDATE: {last_update}\n", "#INFORMATION_B\n"]
         f = open(table_folder + f'/0021.lr2folder', 'w')
-    elif int(level) <= 9:
-        f = open(table_folder + f'/000{level}.lr2folder', 'w')
+
+    elif level == 'OLD':
+        lr2folder = [f"#COMMAND song.hash   in (SELECT md5 FROM Aery WHERE folder = 'OLD CHARTS') \n", "#MAXTRACKS 0\n", "#CATEGORY [5KEYS AERY]\n", f"#TITLE OLD CHARTS\n", f"#INFORMATION_A LAST UPDATE: {last_update}\n", "#INFORMATION_B\n"]
+        f = open(table_folder + f'/0022.lr2folder', 'w')
+        
     else:
-        f = open(table_folder + f'/00{level}.lr2folder', 'w')
+        lr2folder = [f"#COMMAND song.hash   in (SELECT md5 FROM Aery WHERE folder = 'LEVEL {level}') \n", "#MAXTRACKS 0\n", "#CATEGORY [5KEYS AERY]\n", f"#TITLE LEVEL {level}\n", f"#INFORMATION_A LAST UPDATE: {last_update}\n", "#INFORMATION_B\n"]
+        if int(level) <= 9:
+            f = open(table_folder + f'/000{level}.lr2folder', 'w')
+            
+        else:
+            f = open(table_folder + f'/00{level}.lr2folder', 'w')
         
     f.writelines(lr2folder)
     f.close()
